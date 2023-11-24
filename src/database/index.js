18b9cf86df4a33e6d994d88851ce3147cc08afef
vinicius <vinicius.henrique.92@hotmail.com>
2023-11-24 08:56:13 -0300

    fix: database connection

--- src/database/index.js
+++ src/database/index.js
@@ -2,9 +2,7 @@ const { Client } = require('pg');
 
 const client = new Client({
   connectionString: process.env.DATABASE_URL,
-  ssl: {
-    require: true,
-  },
+  ssl: 'require',
 });
 
 client.connect();
