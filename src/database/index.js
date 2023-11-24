a5092585e06a7101866af7889375f20260f63fcc
vinicius <vinicius.henrique.92@hotmail.com>
2023-11-24 10:01:36 -0300

    fix: trying again

--- src/database/index.js
+++ src/database/index.js
@@ -1,19 +1,34 @@
-const { Pool } = require('pg');
-require('dotenv').config();
+const { Client } = require('pg');
 
-const pool = new Pool({
+const client = new Client({
   connectionString: process.env.DATABASE_URL,
-  ssl: {
-    require: true,
-  },
+  ssl: true,
+
 });
 
+client.connect();
+
 exports.query = async (query, values) => {
-  const client = await pool.connect();
-  try {
-    const { rows } = await client.query(query, values);
-    return rows;
-  } finally {
-    client.release();
-  }
+  const { rows } = await client.query(query, values);
+  return rows;
 };
+
+// const { Pool } = require('pg');
+// require('dotenv').config();
+
+// const pool = new Pool({
+//   connectionString: process.env.DATABASE_URL,
+//   ssl: {
+//     require: true,
+//   },
+// });
+
+// exports.query = async (query, values) => {
+//   const client = await pool.connect();
+//   try {
+//     const { rows } = await client.query(query, values);
+//     return rows;
+//   } finally {
+//     client.release();
+//   }
+// };
